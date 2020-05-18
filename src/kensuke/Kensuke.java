50da4b5e393e0c7393b52f80030bb0e52a303ae6
niwa_reina <niwa_reina@192.168.0.192>
2020-05-18 22:23:49 +0900

    チョコVS餃子

--- src/kensuke/Kensuke.java
+++ src/kensuke/Kensuke.java
@@ -4,6 +4,7 @@ public class Kensuke {
 	public static void main(String[] args) {
 		System.out.println("好きな食べ物はチョコレートです。理由は甘くて美味しいからです。");
 		System.out.println("高橋健介がかわいい");
+		System.out.println("丹羽もチョコは美味しいので好きです。");
 	}
 
 }
