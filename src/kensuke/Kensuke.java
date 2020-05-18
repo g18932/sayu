c4156bfecf008ba0f33c6713c2500a31a536b2bd
sym_ishita <sym_ishita@192.168.0.12>
2020-05-18 18:13:36 +0900

    kensuke

--- src/kensuke/Kensuke.java
+++ src/kensuke/Kensuke.java
@@ -3,6 +3,7 @@ package kensuke;
 public class Kensuke {
 	public static void main(String[] args) {
 		System.out.println("好きな食べ物はチョコレートです。理由は甘くて美味しいからです。");
+		System.out.println("高橋健介がかっこいい");
 	}
 
 }
