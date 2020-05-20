f12578b4daac59c283ec187a33e934593ae2bab9
onomoegi <onomoegi@192.168.10.112>
2020-05-20 15:55:34 +0900

    hashimo

--- src/kensuke/Kensuke.java
+++ src/kensuke/Kensuke.java
@@ -4,6 +4,7 @@ public class Kensuke {
 	public static void main(String[] args) {
 		System.out.println("好きな食べ物はチョコレートです。理由は甘くて美味しいからです。");
 		System.out.println("高橋健介がかわいい");
+		System.out.println("私はイチゴ味のチョコが好きです");
 	}
 
 }
